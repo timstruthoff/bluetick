d1ae001b7a84c6c1741676bfad7f1b872e8adab1
Tim Struthoff <tim.struthoff@gmail.com>
2017-04-12 17:31:05 +0200

    various

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080
+
 const app = express();
 app.use(morgan('dev')); // Middleware for logging requests to the console
 
@@ -12,6 +16,6 @@ router.get('/', function(req, res) {
 
 app.use('/', router);
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
     console.log('Express server listening on port ' + server.address().port);
 });
